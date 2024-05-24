bf8af5becf73e4259d82b275a0bb216aabe01b1e
Eluyui3000 <cristofer.contreras.a@mail.pucv.cl>
2024-05-24 00:10:12 -0400

    agregar e historial

--- frontend/src/pages/AgregarComida.tsx
+++ frontend/src/pages/AgregarComida.tsx
@@ -1,68 +1,106 @@
-import React, { useState } from 'react'
-import { IonPage, IonTitle,IonContent,IonHeader,IonToolbar,IonButtons, IonButton, IonItem, IonLabel, IonInput, IonGrid, IonRow, IonCol  } from '@ionic/react'
-import MostrarComidas from '../components/MostrarComidas'
-import {useHistory} from 'react-router-dom'; // Importa useHistory para manejar la navegación
+import React from 'react';
+import ComidaCard, { ComidaType} from '../components/ComidaCard';
+
+import { IonPage, IonTitle, IonContent, IonHeader, IonToolbar, IonButton, IonInput, IonItem, IonLabel } from '@ionic/react';
+
+import { IonIcon } from '@ionic/react';
+import { addCircleOutline } from 'ionicons/icons';
 
 const AgregarComida: React.FC = () => {
-  const history = useHistory(); // Obtiene el objeto history para manejar la navegación
-  const [comidas, setComidas] = useState([]);
-  const [nombre, setNombre] = useState('');
-  const [descripcion, setDescripcion]= useState('');
-
-  const handleAgregarComida = () => {
-    if(!nombre || !descripcion){
-      alert('Por favor ingrese el nombre y la descripción de la comida.');
-      return;
-    }
-    // Agregar la comida a la lista de comidas
-    const nuevaComida = {
-      nombre: nombre,
-      descripcion: descripcion
-    };
-    setComidas([...comidas, nuevaComida]);
-
-    // Limpiar los campos de texto
-    setNombre('');
-    setDescripcion('');
-
-  }
-  const handleVolver=() => {
-    history.goBack(); // Función para volver a la página anterior
+  // Función para manejar el envío del formulario
+  const handleAgregarComidaSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    // Aquí puedes agregar la lógica para procesar los datos del formulario
+    console.log("Se ha enviado el formulario para agregar una nueva comida");
   };
 
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonButtons slot="start">
-            <IonButton onClick={() => history.goBack()}>Volver</IonButton>
-          </IonButtons>
           <IonTitle>Agregar Comida</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <MostrarComidas comidas={comidas} />
-      <IonContent className="ion-padding">
-        <p>Miren, todo esto tenemos para agregar, es humilde pero trabajo honesto :3</p>
-        <IonItem>
-          <IonLabel position="floating">Nombre de la comida</IonLabel>
-          <IonInput value={nombre} onIonChange={(e) => setNombre(e.detail.value!)}></IonInput>
-        </IonItem>
-        <IonItem>
-          <IonLabel position="floating">Descripción de la comida</IonLabel>
-          <IonInput value={descripcion} onIonChange={(e) => setDescripcion(e.detail.value!)}></IonInput>
-        </IonItem>
-        <IonButton expand="block" onClick={handleAgregarComida}>Agregar comida</IonButton>
-        <IonGrid>
-          <IonRow className="ion-justify-content-center">
-            <IonCol size="auto">
-              <IonButton onClick={() => console.log('Agregar otro alimento')}>Agregar otro alimento</IonButton>
-            </IonCol>
-          </IonRow>
-        </IonGrid>
-        
-      </IonContent>
+      <IonContent>
+        <form onSubmit={handleAgregarComidaSubmit}>
+          <IonItem lines="none" style={{ maxWidth: '300px', marginLeft: '20px' }}>
+            <IonLabel position="floating">Ingrese nombre Comida</IonLabel>
+            <br/>
+            <IonInput type="text" required></IonInput>
+          </IonItem>
+          <IonItem lines="none" style={{ maxWidth: '300px', marginLeft: '20px' }}>
+            <IonLabel position="floating">Ingrese cantidad comida</IonLabel>
+            <br/>
+            <IonInput type="number" required></IonInput>
+          </IonItem>
+          <IonButton type="submit" expand="block">
+            <IonIcon icon={addCircleOutline} slot="start" />
+            Agregar Una Comida
+          </IonButton>
+        </form>
+        {comidasAmostrar.map((comida) => (
+          <ComidaCard
+          key={comida.id} comida={comida}/>
+        ))}
+      </IonContent> 
     </IonPage>
   );
 };
 
+
+const comidasAmostrar: Array<{
+  title: string;
+  id: number;
+  type: ComidaType;
+  image: string;
+  calorias:number;
+  proteinas:number;
+  grasas:number;
+  carbohidratos:number;
+}> = [
+  {
+      title: 'Filete de Cerdo',
+      id: 1,
+      type: 'carnita',
+      image:
+          'https://st3.depositphotos.com/10614052/34584/i/450/depositphotos_345848668-stock-photo-cooked-pork-steaks-on-white.jpg',
+      calorias: 430,
+      proteinas: 40,
+      grasas: 10,
+      carbohidratos: 5,
+  },
+  {
+      title: 'Manzana',
+      id: 2,
+      type: 'fruta',
+      image:
+          'https://www.recetasnestlecam.com/sites/default/files/2022-04/tipos-de-manzana-royal-gala.jpg',
+      calorias: 50,
+      proteinas: 2,
+      grasas: 5,
+      carbohidratos: 60,
+  },
+  {
+      title: 'Vaso de Leche',
+      id: 3,
+      type: 'lacteo',
+      image: 
+          'https://img.freepik.com/fotos-premium/vaso-leche-aislado-blanco_62856-4586.jpg',
+      calorias: 100,
+      proteinas: 10,
+      grasas: 5,
+      carbohidratos: 50,
+  },
+  {
+      title: 'Frutilla',
+      id: 4,
+      type: 'fruta',
+      image: 
+          'https://directodelavega.cl/wp-content/uploads/2020/05/frutilla-kilo.jpeg',
+      calorias: 1000,
+      proteinas: 1,
+      grasas: 5,
+      carbohidratos: 20,
+  }
+]
 export default AgregarComida;
--- frontend/src/pages/HistorialComidas.tsx
+++ frontend/src/pages/HistorialComidas.tsx
@@ -1,22 +1,147 @@
-import { IonPage, IonTitle,IonContent,IonHeader,IonToolbar,IonButtons } from '@ionic/react'
+import { IonPage, IonTitle,IonContent,IonHeader,IonToolbar,IonButton } from '@ionic/react'
+import ComidaCard, { ComidaType} from '../components/ComidaCard';
 import React from 'react'
 import MostrarComidas from '../components/MostrarComidas'
-const HistorialComidas: React.FC = () => {
-  return (
-    <div>
-      <IonPage>
-        <IonHeader>
-            <IonToolbar>
-            <IonTitle>Historial de Comidas</IonTitle>
-            </IonToolbar>
-        </IonHeader>
-        <IonContent>
-          Ola muy wenas, aki va el historial de comidas con el editar obviamente
 
-          Falta poner un botón o algo así pa devolverse al nav/comidas ono¿
-        </IonContent>
-      </IonPage>
-    </div>
-  )
-}
+type ComidaType = {
+  title: string;
+  id: number;
+  type: 'fruta' | 'carnita' | 'lacteo'; // Esto es un ejemplo, ajusta según tus necesidades
+  image: string;
+  calorias: number;
+  proteinas: number;
+  grasas: number;
+  carbohidratos: number;
+};
+
+const HistorialComidas: React.FC = () => {
+  const comidasPorSeccion: Record<string, ComidaType[]> = {
+    mañana: [
+      // Aquí puedes agregar las comidas para la mañana
+      {
+        title: 'Manzana',
+        id: 1,
+        type: 'fruta',
+        image:
+          'https://www.recetasnestlecam.com/sites/default/files/2022-04/tipos-de-manzana-royal-gala.jpg',
+        calorias: 50,
+        proteinas: 2,
+        grasas: 5,
+        carbohidratos: 60,
+      },
+      {
+        title: 'Manzana',
+        id: 2,
+        type: 'fruta',
+        image:
+          'https://www.recetasnestlecam.com/sites/default/files/2022-04/tipos-de-manzana-royal-gala.jpg',
+        calorias: 50,
+        proteinas: 2,
+        grasas: 5,
+        carbohidratos: 60,
+      },
+    ],
+    tarde: [
+      {
+        title: 'Filete de Cerdo',
+        id: 1,
+        type: 'carnita',
+        image:
+          'https://st3.depositphotos.com/10614052/34584/i/450/depositphotos_345848668-stock-photo-cooked-pork-steaks-on-white.jpg',
+        calorias: 430,
+        proteinas: 40,
+        grasas: 10,
+        carbohidratos: 5,
+      },
+      // Aquí puedes agregar las comidas para la tarde
+    ],
+    noche: [
+      // Aquí puedes agregar las comidas para la noche
+    ],
+  };
+
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Historial de Comidas</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent>
+        <IonButton expand="block" routerLink="/comidas">Volver a Comidas</IonButton>
+        <div style={{ padding: '10px' }}>
+          <h2>Mañana</h2>
+          {comidasPorSeccion.mañana.map((comida) => (
+            <ComidaCard key={comida.id} comida={comida} />
+          ))}
+          <h2>Tarde</h2>
+          {comidasPorSeccion.tarde.map((comida) => (
+            <ComidaCard key={comida.id} comida={comida} />
+          ))}
+          <h2>Noche</h2>
+          {comidasPorSeccion.noche.map((comida) => (
+            <ComidaCard key={comida.id} comida={comida} />
+          ))}
+        </div>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+
+const comidasAmostrar: Array<{
+  title: string;
+  id: number;
+  type: ComidaType;
+  image: string;
+  calorias:number;
+  proteinas:number;
+  grasas:number;
+  carbohidratos:number;
+}> = [
+  {
+      title: 'Filete de Cerdo',
+      id: 1,
+      type: 'carnita',
+      image:
+          'https://st3.depositphotos.com/10614052/34584/i/450/depositphotos_345848668-stock-photo-cooked-pork-steaks-on-white.jpg',
+      calorias: 430,
+      proteinas: 40,
+      grasas: 10,
+      carbohidratos: 5,
+  },
+  {
+      title: 'Manzana',
+      id: 2,
+      type: 'fruta',
+      image:
+          'https://www.recetasnestlecam.com/sites/default/files/2022-04/tipos-de-manzana-royal-gala.jpg',
+      calorias: 50,
+      proteinas: 2,
+      grasas: 5,
+      carbohidratos: 60,
+  },
+  {
+      title: 'Vaso de Leche',
+      id: 3,
+      type: 'lacteo',
+      image: 
+          'https://img.freepik.com/fotos-premium/vaso-leche-aislado-blanco_62856-4586.jpg',
+      calorias: 100,
+      proteinas: 10,
+      grasas: 5,
+      carbohidratos: 50,
+  },
+  {
+      title: 'Frutilla',
+      id: 4,
+      type: 'fruta',
+      image: 
+          'https://directodelavega.cl/wp-content/uploads/2020/05/frutilla-kilo.jpeg',
+      calorias: 1000,
+      proteinas: 1,
+      grasas: 5,
+      carbohidratos: 20,
+  }
+]
 export default HistorialComidas
